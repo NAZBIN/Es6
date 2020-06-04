ad88283cce619637885d81f9dfc12ad0f9d563bd
qiuwenbin <qiuwenbin@xiaomi.com>
2020-06-04 14:47:55 +0800

    feat：ways to create obj  / deep clone - 6.4

---
+++ JS-training/创建对象.js
@@ -0,0 +1,4 @@
+`1.let obj = new Object
+ 2.let obj = {}
+ 3.let obj = Object.create()
+`
--- JS-training/深克隆.js
+++ JS-training/深克隆.js
@@ -4,3 +4,4 @@ const copyVision = JSON.parse(JSON.stringify(obj));
 `局限性: 1.无法克隆函数等特殊对象 `
 `       2.无法解决循环应用的问题`
 
+`递归实现克隆`
