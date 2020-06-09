44ba5a8c66b7a43bc83628be35ec17d28cd04f73
qiuwenbin <qiuwenbin@xiaomi.com>
2020-06-09 10:14:49 +0800

    utils 6.9

---
+++ JS-training/判断一个对象是否为空.js
@@ -0,0 +1,7 @@
+//1. Es6的 Object.keys() 
+
+`返回一个自身对象的可枚举属性组成的数组`
+
+//2 Json.stringify
+
+if( JSON.stringify(obj) === '{}') return false;  //把json转为字符串来判断
---
+++ JS-training/计算字符串字节数.js
@@ -0,0 +1,5 @@
+const computeLens = (s) => {
+    return s.split('').length
+}
+
+console.log(computeLens('aaacvsa'));
